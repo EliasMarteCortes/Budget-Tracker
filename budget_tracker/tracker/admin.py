a5b4ede5ab8a0a3cffb1a67127e0b8b6f39b7fce
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-05-12 17:01:51 -0400

    feat: register models in django admin

--- budget_tracker/tracker/admin.py
+++ budget_tracker/tracker/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Transaction, Budget
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'transaction_type', 'category', 'date', 'note')
+    list_filter = ('transaction_type', 'category')
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('category', 'limit', 'month', 'year')
