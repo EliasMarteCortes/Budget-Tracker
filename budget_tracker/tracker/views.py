c6dec07b4b994f276dde1f33dac0e86b8299c112
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-05-12 20:07:28 -0400

    feat: add dashboard view

--- budget_tracker/tracker/views.py
+++ budget_tracker/tracker/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from .models import Transaction
 
-# Create your views here.
+def dashboard(request):
+    transactions = Transaction.objects.all()
+
+    total_income = (
+        transactions
+        .filter(transaction_type='income')
+        .aggregate(total=Sum('amount'))['total'] or 0
+    )
+
+    total_expense = (
+        transactions
+        .filter(transaction_type='expense')
+        .aggregate(total=Sum('amount'))['total'] or 0
+    )
+
+    balance = total_income - total_expense
+
+    category_totals = (
+        transactions
+        .filter(transaction_type='expense')
+        .values('category__name')
+        .annotate(total=Sum('amount'))
+        .order_by('-total')
+    )
+
+    context = {
+        'transactions': transactions[:10],
+        'total_income': total_income,
+        'total_expense': total_expense,
+        'balance': balance,
+        'category_totals': category_totals,
+    }
+
+    return render(request, 'tracker/dashboard.html', context)
