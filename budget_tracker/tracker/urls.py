8820e42cb6280d7713b74da5a9255055fa5c54b5
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-05-15 18:40:05 -0400

    feat: add delete category url

--- budget_tracker/tracker/urls.py
+++ budget_tracker/tracker/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('transactions/edit/<int:pk>/', views.edit_transaction, name='edit_transaction'),
     path('transactions/delete/<int:pk>/', views.delete_transaction, name='delete_transaction'),
     path('categories/', views.category_list, name='category_list'),
+    path('categories/delete/<int:pk>/', views.delete_category, name='delete_category'),
 ]
